c969c353f7e59d6f5432c48be15eedbc93e98293
milrosen <61394098+milrosen@users.noreply.github.com>
2023-01-19 18:43:53 -0500

    added css to pages

--- src/pages/blurb/index.jsx
+++ src/pages/blurb/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './blurb.css'
 
 export default function blurb() {
 	return (
-		<div className='page blurb'>
-			<div>My name is milton rosenbaum</div>
+		<div className='page blurb bottom-right'>
+			<div>My name is Milton Rosenbaum</div>
 			<div> I design beautifully simple things</div>
 			<div>and find the art in algorithms data</div>
 		</div>
--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import './projects.css'
+
+const pro = [
+	{ link: '/', name: 'Sienfield Theme', desc: 'Im claiming I made the Sienfield theme.' }
+];
 
 export default function projects() {
 	return (
-		<div>projects</div>
+		<div className='page bottom-right project'>
+			<div>Projects</div>
+			{pro.map(({ link, desc, name }) => {
+				return (
+					<div>
+						<h2>{name}</h2>
+						<div>{desc}</div>
+					</div>
+				)
+			})}
+		</div>
 	)
 }
