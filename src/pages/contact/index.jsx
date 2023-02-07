d792571336da0350a6ad07fbbcc7c97b4d8f702b
milrosen <61394098+milrosen@users.noreply.github.com>
2023-02-07 16:19:26 -0500

    adding email functionality

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,9 +1,60 @@
+import './contact.css'
 import React from 'react'
 
-export default function contact() {
-	return (
-		<div>
-			contact
-		</div>
-	)
+
+export default class Contact extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { name: '', email: '', message: '', displayConfirmation: false };
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		const target = event.target;
+		const value = target.value;
+		const name = target.name;
+		this.setState({
+			[name]: value
+		});
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+		window.emailjs.send("service_q1zb69y", "template_82r2p6f", {
+			contact_name: this.state.name,
+			contact_email: this.state.email,
+			message: this.state.message,
+		}).then(() => {
+			this.setState({ displayConfirmation: true })
+		});
+		this.setState({
+			name: '',
+			email: '',
+			message: '',
+		})
+	}
+
+	render() {
+		return (
+			<div className='page bottom-right'>
+				<div className={`confirmation${this.state.displayConfirmation ? ' display' : ''}`}>email sent</div>
+				<form onSubmit={this.handleSubmit}>
+					<label>
+						Name:
+						<input required name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+					</label>
+					<label>
+						Email:
+						<input required name="email" type="email" value={this.state.email} onChange={this.handleChange} />
+					</label>
+					<label>
+						Message:
+						<textarea required name="message" value={this.state.message} onChange={this.handleChange} />
+					</label>
+					<input type="submit" value="Submit" />
+				</form>
+			</div>
+		);
+	}
 }
--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 import './projects.css'
 
 const pro = [
-	{ link: '/', name: 'Sienfield Theme', desc: 'Im claiming I made the Sienfield theme.' }
+	{ link: 'milrosen.github.io/pfaucet', name: 'Plumber\'s Faucet', desc: 'Designed and developed the website for Mcgill\'s premier humor magazine' }
 ];
 
 export default function projects() {
 	return (
 		<div className='page bottom-right project'>
 			<div>Projects</div>
-			{pro.map(({ link, desc, name }) => {
+			{pro.map(({ link, desc, name }, index) => {
 				return (
-					<div>
+					<a key={index} href={link}>
 						<h2>{name}</h2>
 						<div>{desc}</div>
-					</div>
+					</a>
 				)
 			})}
 		</div>
